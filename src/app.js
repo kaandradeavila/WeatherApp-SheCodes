79b5cc62aa5453b20d6f9d8077e8652603ce4213
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2024-06-07 17:27:12 -0400

    added axios in the index.html file, and the API data and request to the index.js
    (displayTemperature() not defined yet)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
+function displayTemperature(response){
+    
+}
+
+function apiTemperatureRequest(){
+    let apiKey = "424369doa037d0347bft3cfcc8cef956";
+    let city = "";
+    let apiUrl = "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+
+    axios.get(apiUrl).then(displayTemperature);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
 
     let searchInput = document.querySelector("#search-form-input");
-    let cityName = document.querySelector("#city");
+    let cityElement = document.querySelector("#city");
 
-    cityName.innerHTML = searchInput.value;
+    cityElement.innerHTML = searchInput.value;
+
+    apiTemperatureRequest();
 }
 
 let searchForm = document.querySelector("#search-form");
