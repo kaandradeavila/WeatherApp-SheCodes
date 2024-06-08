a82958df61e6ad840f49a596f54775d41e27857d
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2024-06-07 20:04:12 -0400

    added displayWeatherTemperature() definition and displayed it in the app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-function displayWeatherTemperature(){
+function displayWeatherTemperature(currentTemperature){
+    let currentTemperatureElement = document.querySelector("#weather-temp");
+    currentTemperatureElement.innerHTML = Math.round(currentTemperature);
 }
 
 function displayWeatherCondition(){
@@ -13,22 +15,23 @@ function displayWeatherDay(){
 
 }
 
-function displayDate()[
+function displayDate(){
 
-]
+}
 
 function displayTime(){
-    
+
 }
 
 function getData(response){
-
+    console.log(response);
+    displayWeatherTemperature(response.data.temperature.current);
 }
 
 function apiTemperatureRequest(city){
     let units = "imperial";
     let apiKey = "424369doa037d0347bft3cfcc8cef956";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=${units}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=imperial`;
 
     axios.get(apiUrl).then(getData);
 }
